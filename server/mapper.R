1d71261d227f7ac12a031a1cee269a9665a788bd
Bogdan Rau <bogdan.rau@gmail.com>
2015-06-07 04:54:04 -0700

    Fixed map display issue in production.

--- server/mapper.R
+++ server/mapper.R
@@ -1,18 +1,7 @@
 output$myMap <- renderLeaflet({
     input$getData
 #     mapData <- isolate(dbGetQuery(connection, paste0("SELECT zip, BMA_20 FROM data")))
-    
-    
     latlon <- geocode(as.character(input$demographicZip))
-    
-    
-    pal <- colorQuantile("YlGn", NULL, n = 5)
-    popup <- paste0("<strong>Zip: </strong>", 
-                          data$zip, 
-                          "<br><strong>BMA_20: </strong>", 
-                          data$BMA_20)
-    
 
-    
-    m = leaflet() %>% addProviderTiles("Stamen.TonerLite", options = providerTileOptions(noWrap = TRUE)) %>% setView(latlon$lon, latlon$lat, zoom = 10)
+    leaflet() %>% addProviderTiles("Stamen.TonerLite", options = providerTileOptions(noWrap = TRUE)) %>% setView(latlon$lon, latlon$lat, zoom = 10)
 })
